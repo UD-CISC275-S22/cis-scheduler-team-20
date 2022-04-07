97b9dc5a67ad2da4b2a788362d9bde28e1401c46
Harrison Littlepage <harrisonlittlepage@wifi-roaming-128-4-139-106.wifi.udel.edu>
2022-04-07 15:06:46 -0400

    changed the header

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,11 @@ function App(): JSX.Element {
         <div className="App">
             <header className="App-header">
                 <img
+                    className="logo"
                     src="https://fanapeel.com/wp-content/uploads/logo_-university-of-delaware-fightin-blue-hens-ud.png"
                     alt="a UD logo"
                 />
-                Welcome To The UD Scheduler
+                Welcome To The UD Degree Plan Scheduler
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
