bdfa2e250c6f991140517a3ea9944874ca15e611
Harrison Littlepage <harrisonlittlepage@wifi-roaming-128-4-139-106.wifi.udel.edu>
2022-04-07 14:41:51 -0400

    logo

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">Welcome To The UD Scheduler</header>
+            <img
+                src="https://fanapeel.com/wp-content/uploads/logo_-university-of-delaware-fightin-blue-hens-ud.png"
+                alt="a UD logo"
+            />
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
