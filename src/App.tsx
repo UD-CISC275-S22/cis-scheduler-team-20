0b504b51dbd16ea292049b6ea9374302c950f4a4
Harrison Littlepage <harrisonlittlepage@Harrisons-MBP-2.fios-router.home>
2022-04-07 19:18:56 -0400

    just added divs around the header and picture

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,16 @@ import { AddSemester } from "./components/AddSemester";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                <img
-                    className="logo"
-                    src="https://fanapeel.com/wp-content/uploads/logo_-university-of-delaware-fightin-blue-hens-ud.png"
-                    alt="a UD logo"
-                />
-                Welcome To The UD Degree Plan Scheduler
-            </header>
+            <div>
+                <header className="App-header">
+                    <img
+                        className="logo"
+                        src="https://fanapeel.com/wp-content/uploads/logo_-university-of-delaware-fightin-blue-hens-ud.png"
+                        alt="a UD logo"
+                    />
+                    Welcome To The UD Degree Plan Scheduler
+                </header>
+            </div>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
