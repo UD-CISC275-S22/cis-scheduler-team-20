961b7a549ce905b40eb4cf43a74b14095dae061e
Ethan S <estipes@udel.edu>
2022-04-12 15:09:45 -0400

    Fixed Warnings in Add Semester

--- src/components/AddSemester.tsx
+++ src/components/AddSemester.tsx
@@ -1,15 +1,10 @@
 import React, { ReactElement, useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 export function AddSemester(): JSX.Element {
     const [semesters, setSemesters] = useState<ReactElement[]>([]);
     const [num, setNum] = useState<number>(-1);
     const courses = ["CISC108", "EGGG101", "MATH241", "ENGL110"];
-    const [course, setCourse] = useState<string>(courses[0]);
-    function changeCourse(event: React.ChangeEvent<HTMLSelectElement>): string {
-        setCourse(event.target.value);
-        return event.target.value;
-    }
     function insertSemester(): void {
         setNum(num + 1);
         setSemesters([
@@ -39,25 +34,58 @@ export function AddSemester(): JSX.Element {
                             <td>Not Taken</td>
                         </tr>
                         <tr>
-                            <td>Math241</td>
+                            <td>
+                                <select>
+                                    {courses.map((cours: string) => (
+                                        <option key={cours} value={cours}>
+                                            {cours}
+                                        </option>
+                                    ))}
+                                </select>
+                            </td>
                             <td>Calc and Analytics A</td>
                             <td>3</td>
                             <td>Not Taken</td>
                         </tr>
                         <tr>
-                            <td>Eng110</td>
+                            <td>
+                                <select>
+                                    {courses.map((cours: string) => (
+                                        <option key={cours} value={cours}>
+                                            {cours}
+                                        </option>
+                                    ))}
+                                </select>
+                            </td>
                             <td>College English</td>
                             <td>3</td>
                             <td>Not Taken</td>
                         </tr>
                         <tr>
-                            <td>Hist requirement(1/2)</td>
+                            <td>
+                                <select>
+                                    {courses.map((cours: string) => (
+                                        <option key={cours} value={cours}>
+                                            {cours}
+                                        </option>
+                                    ))}
+                                </select>
+                                )
+                            </td>
                             <td>N/A</td>
                             <td>3</td>
                             <td>Not Taken</td>
                         </tr>
                         <tr>
-                            <td>EGGG101</td>
+                            <td>
+                                <select>
+                                    {courses.map((cours: string) => (
+                                        <option key={cours} value={cours}>
+                                            {cours}
+                                        </option>
+                                    ))}
+                                </select>
+                            </td>
                             <td>Introduction to Engineering</td>
                             <td>3</td>
                             <td>Not Taken</td>
