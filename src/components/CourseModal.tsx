a46a62285a16af91a3c85b7bc79e8725216afa95
Harrison Littlepage <harrisonlittlepage@wifi-roaming-128-4-107-204.wifi.udel.edu>
2022-05-10 14:41:32 -0400

    just added a testid

--- src/components/CourseModal.tsx
+++ src/components/CourseModal.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import { Modal } from "react-bootstrap";
 import { Course } from "../Interfaces/course";
 import { Semester } from "../Interfaces/semester";
-import { AddCoursetoSemester } from "./AddCoursetoSemester";
-
+import { AddCoursetoSemester } from "./AddCourseToSemester";
 //holds the modal and brings up the AddCourseToSemester UI in a pop-up window
 //Wee
 
