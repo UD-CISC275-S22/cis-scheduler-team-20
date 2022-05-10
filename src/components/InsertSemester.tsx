d200d038ee4b8d172358ded38cf54012b153db03
Harrison Littlepage <harrisonlittlepage@wifi-roaming-128-4-107-204.wifi.udel.edu>
2022-05-10 14:43:19 -0400

    I made insert semester take ina semester

--- src/components/InsertSemester.tsx
+++ src/components/InsertSemester.tsx
@@ -1,25 +1,30 @@
 import React, { useState } from "react";
 import { origionalPlan } from "../Interfaces/origionalPlan";
 import { Button, Col, Form, Modal, Row } from "react-bootstrap";
+import { Semester } from "../Interfaces/semester";
 
 //Adds a semester to a specific plan
 export function InsertSemesterModal({
     showModal,
-    closeModal
+    closeModal,
+    semester
 }: {
+    semester: (newSemester: Semester) => void;
     showModal: boolean;
     closeModal: () => void;
 }): JSX.Element {
     const [year, setYear] = useState<number>(2022);
     const [season, setSeason] = useState<string>("Fall");
-    function addSem(yr: number, seas: string): void {
-        origionalPlan.semesters.splice(origionalPlan.semesters.length, 0, {
-            id: "1",
-            year: yr,
-            season: seas,
-            classes: [...origionalPlan.semesters[0].classes],
-            credits: 3
+    const [id, setId] = useState<string>(" ");
+    function addSem(): void {
+        semester({
+            id: id,
+            year: year,
+            season: season,
+            classes: [],
+            credits: 0
         });
+        closeModal();
     }
     return (
         <Modal show={showModal} onHide={closeModal} animation={false}>
@@ -70,7 +75,7 @@ export function InsertSemesterModal({
                 <Button
                     data-testid="saveSemesterButton"
                     variant="primarmy"
-                    onClick={() => addSem(year, season)}
+                    onClick={addSem}
                 >
                     Add Semester
                 </Button>
